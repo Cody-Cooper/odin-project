c1428b5089f433f302ca50c0399765f59f0ff8bf
Cody Cooper <contact@itscodycooper.com>
2015-11-27 19:43:29 -0800

    Added solution

--- project-euler/1-3/js/primefactor.js
+++ project-euler/1-3/js/primefactor.js
@@ -1,1 +1,14 @@
-// Working on it
+var userInput = 20;
+var answer = userInput;
+var i = 2;
+while(i<answer) {
+	while(answer%i === 0) {
+		(function(tempAnswer) {
+			document.write(answer + " can be divided by " + i + " which gives us " + tempAnswer + "<br><br>");
+
+			answer = tempAnswer;
+		})(answer / i)
+	}
+	i++;
+}
+document.write(answer + " is the biggest prime factor for " + userInput + ".");
